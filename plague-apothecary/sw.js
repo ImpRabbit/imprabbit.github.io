f313b35edbf13f7845858c55f189b20bb791ac6e
ImpRabbit <brags.masher_5g@icloud.com>
2026-07-12 22:47:28 +0900

    Fix mobile cache refresh for plague apothecary

--- plague-apothecary/sw.js
+++ plague-apothecary/sw.js
@@ -1,5 +1,5 @@
-const CACHE='tiny-apothecary-standalone-v1';
+const CACHE='tiny-apothecary-standalone-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})))})
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./',copy));return response}).catch(()=>caches.match('./')));return}event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)))})
