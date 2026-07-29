42831eed0c5a15616342dcadd07851568bb124ac
ImpRabbit <brags.masher_5g@icloud.com>
2026-07-29 22:55:27 +0900

    admiral-chess: sw and manifest

---
+++ admiral-chess/sw.js
@@ -0,0 +1,47 @@
+const CACHE = "admiral-chess-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./lessons.html",
+  "./play.html",
+  "./game.html",
+  "./settings.html",
+  "./css/style.css",
+  "./js/chess-board.js",
+  "./js/progress.js",
+  "./js/simple-ai.js",
+  "./js/stockfish-ai.js",
+  "./manifest.webmanifest",
+  "./images/icon.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetched = fetch(e.request).then((res) => {
+        if (res && res.ok && e.request.method === "GET") {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
